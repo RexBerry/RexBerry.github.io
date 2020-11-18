da1cf22d2fc256c45ae2ade3ba77534ee7e9c1cb
RexBerry <59031902+RexBerry@users.noreply.github.com>
2020-11-18 07:39:59 -0600

    Updated Javascript

--- school/education/main.js
+++ school/education/main.js
@@ -1,11 +1,13 @@
 document.write('School');
 
 function move() {
-  let el = document.getElementById('moving');
-  el.innerText = `This is true education ${Math.round(1000000 * Math.random())}`;
-  el.style.position = 'absolute';
-  el.style.left = `${Math.round(600 * Math.random())}px`;
-  el.style.top = `${Math.round(600 * Math.random())}px`;
+  for (let i = 0; i < 10; i++) {
+    let el = document.getElementById(`moving${i}`);
+    el.innerText = `This is true education ${Math.round(1000000 * Math.random())}`;
+    el.style.position = 'absolute';
+    el.style.left = `${Math.round(600 * Math.random())}px`;
+    el.style.top = `${Math.round(600 * Math.random())}px`;
+  }
 }
 
 move();
