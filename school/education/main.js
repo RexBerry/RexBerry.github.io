145c95dc498c316634e07f2e95ba04059e4e785c
RexBerry <59031902+RexBerry@users.noreply.github.com>
2020-11-18 07:29:16 -0600

    Added innerText

--- school/education/main.js
+++ school/education/main.js
@@ -1,6 +1,7 @@
 function move() {
   let el = document.getElementById('moving');
-  el.style.position = "absolute";
+  el.innerText = `This is true education ${Math.round(1000000 * Math.random())}`;
+  el.style.position = 'absolute';
   el.style.left = `${Math.round(600 * Math.random())}px`;
   el.style.top = `${Math.round(600 * Math.random())}px`;
 }
