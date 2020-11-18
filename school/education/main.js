13fc6ee75bb8c64277de62ac122ee51ac2b337e0
RexBerry <59031902+RexBerry@users.noreply.github.com>
2020-11-18 07:49:17 -0600

    Changed movement pattern

--- school/education/main.js
+++ school/education/main.js
@@ -1,15 +1,32 @@
 document.write('School');
 
+tick = 0;
+newPositions = [];
+for (let i = 0; i < 10; i++) {newPositions.push([0, 0]);}
+
 function move() {
+  
+  if (tick % 60 === 0) {
+    oldPositions = newPositions;
+    newPositions = [];
+    for (let i = 0; i < 10; i++) {
+      newPositions.push([Math.round(600 * Math.random()), Math.round(600 * Math.random())]);
+    }
+  }
+  
+  let t = (tick % 60) / 60;
+  
   for (let i = 0; i < 10; i++) {
     let el = document.getElementById(`moving${i}`);
     el.innerText = `This is true education ${Math.round(1000000 * Math.random())}`;
     el.style.position = 'absolute';
-    el.style.left = `${Math.round(600 * Math.random())}px`;
-    el.style.top = `${Math.round(600 * Math.random())}px`;
+    el.style.left = `${(1 - t) * oldPositions[i][0] + t * newPositions[i][0]}px`;
+    el.style.top = `${(1 - t) * oldPositions[i][1] + t * newPositions[i][1]}px`;
   }
+  
+  tick++;
 }
 
 move();
 
-setInterval(move, 10);
+setInterval(move, 1000/60);
