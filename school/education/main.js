4e1d99b8451fced6976789bcd703474ecba6eada
RexBerry <59031902+RexBerry@users.noreply.github.com>
2020-11-18 07:23:12 -0600

    Create main.js

---
+++ school/education/main.js
@@ -0,0 +1,6 @@
+function move() {
+  let el = document.getElementById('moving);
+  el.style = `offset: ${Math.round(600 * Math.random())} ${Math.round(600 * Math.random())};`;
+}
+
+setInterval(move, 10);
