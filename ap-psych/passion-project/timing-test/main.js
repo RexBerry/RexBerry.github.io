e1ff11cefe44dcd7cbe75bc4c21d0e7bd89fd2b6
RexBerry <rexb987@gmail.com>
2021-05-24 21:04:02 -0500

    Add Timing Test

---
+++ ap-psych/passion-project/timing-test/main.js
@@ -0,0 +1,53 @@
+count = 0;
+MAX_CLICKS = 33;
+
+clickTimes = [];
+
+function clickMe() {
+
+    if (count === MAX_CLICKS) {
+
+        if (confirm("Start another trial?")) {
+            count = 0;
+            document.getElementById('count').innerHTML = '';
+        }
+
+        return;
+
+    }
+
+    if (count === 0) {
+        clickTimes = [];
+    }
+    
+    clickTimes.push(performance.now());
+
+    count++;
+    document.getElementById('count').innerHTML = `${count}/${MAX_CLICKS}`;
+
+    if (count === MAX_CLICKS) {
+
+        let deltaTime = [];
+        for (let i = 1; i < clickTimes.length; i++) {
+            deltaTime.push(clickTimes[i] - clickTimes[i - 1]);
+        }
+
+        let mean = 0;
+        for (let i = 0; i < deltaTime.length; i++) {
+            mean += deltaTime[i];
+        }
+        mean /= deltaTime.length;
+
+        let stddev = 0;
+        for (let i = 0; i < deltaTime.length; i++) {
+            stddev += (deltaTime[i] - mean) * (deltaTime[i] - mean);
+        }
+        stddev = Math.sqrt(stddev / (deltaTime.length - 1));
+
+        document.getElementById('meanSpan').innerHTML = `Mean Time Between Clicks: ${(mean / 1000).toFixed(3)} sec`;
+        document.getElementById('stddevSpan').innerHTML = `Standard Deviation: ${(stddev / 1000).toFixed(3)} sec (${(100 * stddev / mean).toFixed(1)}% of mean)`;
+
+    }
+
+
+}
